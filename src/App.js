62618dd3a7c4f9b275325788702117ebd4f16be1
annaavanesyan <anna.avanesyan1998@gmail.com>
2018-06-19 02:41:42 +0400

    adding: wrapping props in state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React, { Component } from "react";
 import "./App.css";
-import Person from './Person/Person';
+import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    people: [
+      { name: "Anna", age: 20 },
+      { name: "Narek", age: 23 },
+      { name: "Bobo", age: 1 }
+    ]
+  };
   render() {
     return (
       <div className="App">
         <h1>Hi, I am a react App</h1>
         <p>probably</p>
-        <Person name="Anna" age="20"> Hobbies: Tennis</Person>
-        <Person name="Narek" age="23"> Hobbies: Gym</Person>
-        <Person name="Bobo" age="1"/>
+        <Person name={this.state.people[0].name} age={this.state.people[0].age}>
+          Hobbies: Tennis
+        </Person>
+        <Person name={this.state.people[1].name} age={this.state.people[1].age}>
+          Hobbies: Gym
+        </Person>
+        <Person name={this.state.people[2].name} age={this.state.people[2].age} />
       </div>
     );
   }
