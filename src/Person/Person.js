28def5f993916ca7f1a4247ca81822cc002fe374
annaavanesyan <anna.avanesyan1998@gmail.com>
2018-06-19 02:33:05 +0400

    adding: person function component

---
+++ src/Person/Person.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const person = (props) => {
+  return (
+    <div className ="separate">
+      <p> My name is: {props.name}, age: {props.age} </p>
+      <p> {props.children} </p>
+    </div>
+  );
+};
+
+export default person;
