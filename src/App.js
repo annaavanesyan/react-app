ec1ed0e6a363ec3106dbee731a9f8027d7376c16
annaavanesyan <anna.avanesyan1998@gmail.com>
2018-06-19 02:33:51 +0400

    using person component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
@@ -7,6 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I am a react App</h1>
         <p>probably</p>
+        <Person name="Anna" age="20"> Hobbies: Tennis</Person>
+        <Person name="Narek" age="23"> Hobbies: Gym</Person>
+        <Person name="Bobo" age="1"/>
       </div>
     );
   }
