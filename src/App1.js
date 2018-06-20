41e3bb8f03a61d5256976e33e7b3adfd7850b832
annaavanesyan <anna.avanesyan1998@gmail.com>
2018-06-21 02:38:30 +0400

    add: event handler manipulating state  - reduce to one username

--- src/App1.js
+++ src/App1.js
@@ -1,14 +1,28 @@
 import React, { Component } from "react";
 import "./App.css";
 import UserOutput from "./User/UserOutput";
+import UserInput from "./User/UserInput";
 
 class UserApp extends Component {
+  state = {
+    user: {
+      username: "empremto"
+    }
+  };
+
+  changeUsernameHandler = event => {
+    this.setState({
+      user: {
+        username: event.target.value
+      }
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <UserOutput username="empremto"/>
-        <UserOutput username="ignite"/>
-        <UserOutput username="super"/>
+        <UserInput changeUsername={this.changeUsernameHandler} />
+        <UserOutput username={this.state.user.username} />
       </div>
     );
   }
--- src/User/UserInput.js
+++ src/User/UserInput.js
@@ -1,8 +1,15 @@
-import React from 'react'
-import "./User.css"
+import React from "react";
+import "./User.css";
 
-const userInput = () => {
-  <input type="text"/>
-}
+const userInput = props => {
+  return (
+    <input
+      className="input"
+      onChange={props.changeUsername}
+      type="text"
+      placeholder="insert username"
+    />
+  );
+};
 
 export default userInput;
