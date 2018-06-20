b6dc0bc0abe6d53f2e81c4b72c82490d365c63c9
annaavanesyan <anna.avanesyan1998@gmail.com>
2018-06-21 03:04:39 +0400

    add: add two-way-binding to UserInput

--- src/User/UserInput.js
+++ src/User/UserInput.js
@@ -3,12 +3,17 @@ import "./User.css";
 
 const userInput = props => {
   return (
-    <input
-      className="input"
-      onChange={props.changeUsername}
-      type="text"
-      placeholder="insert username"
-    />
+    <div>
+      <span>change username</span>
+      <span><u>   {props.username}  </u></span>
+      <span> to </span>
+      <input
+        className="input"
+        onChange={props.changeUsername}
+        type="text"
+        placeholder="insert username"
+      />
+    </div>
   );
 };
 
